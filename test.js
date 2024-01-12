873123de5f6f5003740cf8cb8df7b3ccdd2f4ca0
adolfosucumula <rocketmc2009@gmail.com>
2024-01-12 09:49:03 +0000

    test.js file

---
+++ test.js
@@ -0,0 +1,7 @@
+const expresss = require('express');
+const app = express();
+
+app.get('/', function(req, res)=>{
+    res.set('Content-Type', 'text/html, charset=utf-8');
+    res.send("<h1>Hello world!</h1>");
+})
